11c479b29ac02a4988bcbb6a5e097ea00fc02432
CraZy LegenD <crazyxylene@gmail.com>
2020-04-29 16:41:03 +0200

    feature: new utils and updated kotlin/gradle

--- arcgisextensions/src/main/java/com/crazylegend/arcgisextensions/OtherUtils.kt
+++ arcgisextensions/src/main/java/com/crazylegend/arcgisextensions/OtherUtils.kt
@@ -1,13 +1,25 @@
 package com.crazylegend.arcgisextensions
 
 import android.content.Context
+import com.crazylegend.kotlinextensions.exhaustive
+import com.crazylegend.kotlinextensions.log.debug
+import com.crazylegend.kotlinextensions.tryOrNull
+import com.esri.arcgisruntime.concurrent.ListenableFuture
+import com.esri.arcgisruntime.data.QueryParameters
+import com.esri.arcgisruntime.data.ServiceFeatureTable
 import com.esri.arcgisruntime.data.TileCache
+import com.esri.arcgisruntime.geometry.Envelope
 import com.esri.arcgisruntime.geometry.Geometry
 import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.loadable.LoadStatus
+import com.esri.arcgisruntime.loadable.Loadable
 import com.esri.arcgisruntime.mapping.MobileMapPackage
 import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.MapView
 import com.esri.arcgisruntime.tasks.networkanalysis.Stop
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -59,4 +71,99 @@ fun Context.loadMMPKandTPKfromRAW(mapID: Int, mmpkName: String = "map.mmpk",
             onBothLoadedCallback.invoke(map, tpk)
         }
     }
+}
+
+
+fun GregorianCalendar?.toDate(defaultDateFormat: SimpleDateFormat) = this?.let {
+    defaultDateFormat.format(it.time)
+}
+
+
+fun esriQuery(builder: QueryParameters.() -> Unit): QueryParameters {
+    val queryParameters = QueryParameters()
+    builder.invoke(queryParameters)
+    return queryParameters
+}
+
+fun generateToleranceEnvelope(mapView: MapView, locationPoint: Point): Envelope {
+    val mapTolerance = mapView.getTolerance()
+    return Envelope(locationPoint.x - mapTolerance, locationPoint.y - mapTolerance,
+            locationPoint.x + mapTolerance, locationPoint.y + mapTolerance, mapView.spatialReference)
+}
+
+fun MapView?.getTolerance(tolerance: Double = 10.0): Double {
+    if (this == null) return tolerance
+    return tolerance * this.unitsPerDensityIndependentPixel
+}
+
+
+fun Loadable?.loadDSL(isDebug: Boolean = false, onFailedLoading: () -> Unit = {}, onLoading: () -> Unit = {}, onSuccessfulLoading: () -> Unit = {}): Loadable? {
+    if (this == null) return null
+
+    addDoneLoadingListener {
+        when (loadStatus) {
+            LoadStatus.NOT_LOADED -> {
+                onFailedLoading()
+                onDebug(isDebug) {
+                    debug("NOT_LOADED")
+                }
+            }
+            LoadStatus.LOADING -> {
+                onLoading()
+                onDebug(isDebug) {
+                    debug("LOADING")
+                }
+            }
+            LoadStatus.LOADED -> {
+                onSuccessfulLoading()
+                onDebug(isDebug) {
+                    debug("LOADED")
+                }
+            }
+            LoadStatus.FAILED_TO_LOAD -> {
+                onFailedLoading()
+                onDebug(isDebug) {
+                    debug("FAILED_TO_LOAD ${loadError.errorCode} ${loadError.additionalMessage} ${loadError.localizedMessage}")
+                }
+            }
+
+            null -> {
+                onFailedLoading()
+                onDebug(isDebug) {
+                    debug("null returned when loading")
+                }
+            }
+        }.exhaustive
+        loadError?.cause?.printStackTrace()
+    }
+    loadAsync()
+    return this
+}
+
+fun onDebug(debug: Boolean, function: () -> Any) {
+    if (debug) function()
+}
+
+val MapView?.visibleAreaJSON get() = this?.visibleArea?.extent?.center?.toJson()
+
+fun String?.envelopeFromJson() = tryOrNull { Envelope.fromJson(this).extent.center }
+
+val FeatureLayer?.asServiceFeatureTable get() = this?.featureTable as? ServiceFeatureTable
+
+fun generateQueryWithGeometry(toleranceEnvelope: Envelope): QueryParameters {
+    val query = QueryParameters()
+    query.geometry = toleranceEnvelope
+    return query
+}
+
+val queryEverything get() = esriQuery { whereClause = "1=1" }
+
+fun <V> ListenableFuture<V>?.loadDSL(onSuccessfulLoad: (V?) -> Unit): ListenableFuture<V>? {
+    this?.addDoneListener {
+        val result = tryOrNull {
+            get()
+        }
+        onSuccessfulLoad(result)
+    }
+    return this
 }
